c73b143882fde2a1664af3f647d26763f4912eca
nipunattri1 <67823424+nipunattri1@users.noreply.github.com>
2021-11-24 23:38:56 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View, Alert, Button } from 'react-native';
 import HomeScreen from './src/Screens/HomeSceen';
 
 export default function App() {
@@ -8,4 +7,3 @@ export default function App() {
     <HomeScreen />
   );
 }
-
